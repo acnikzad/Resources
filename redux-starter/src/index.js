199a729100240ff913bbc8e06ea5885d63c3704e
Aria Nikzad <a.c.nikzad@gmail.com>
2023-04-01 12:20:43 -0700

    added arrays

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,4 +1,5 @@
 import {compose, pipe} from 'lodash/fp'
+import {produce} from 'immer'
 
 let username = "  Harley  ";
 let message = "Hello " +username.trim()+ " Good morning";
@@ -21,5 +22,29 @@ const newEmployee = {
     company: {...employee.company, city: "Calgary"}
 }
 
+produce(employee, (draftState) =>{
+    draftState.name = "Jimmy";
+    draftState.company.city = "Calgary";
+})
+
 console.log(employee);
 console.log(newEmployee);
+
+// ***********************************
+// adding items
+const numbers = [10, 20, 30, 40];
+
+const index = numbers.indexOf(30);
+const addedNumbers = [...numbers.slice(0, index), 50, ...numbers.slice(index)];
+console.log(addedNumbers);
+
+// updating items
+const updated = numbers.map((a) => (a === 40 ? 80 : a))
+console.log(updated)
+
+//removing items
+const removed = numbers.filter(a => a !== 30);
+console.log(removed)
+
+// ***********************************
+// exercise
