66a0e273ea779a71b03ec62fdbf606ee19947db7
Aria Nikzad <a.c.nikzad@gmail.com>
2023-04-02 13:42:39 -0700

    fixed bug

--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -1,20 +1,47 @@
-// ***********************************
-// reducers
-let id = 0
-export default function reducer(state =[], action) {
+// Action Types
+const ADD_TASK = "ADD_TASK";
+const REMOVE_TASK = "REMOVE_TASK";
+const TASK_COMPLETED = "TASK_COMPLETED";
 
-    switch(action.type) {
-        case 'ADD_TASK ':
+// Actions
+export const addTask = (task) => {
+    return { type: ADD_TASK, payload: { task: task } };
+};
+
+export const removeTask = (id) => {
+    return { type: REMOVE_TASK, payload: { id: id } };
+};
+
+export const completedTask = (id) => {
+    return { type: TASK_COMPLETED, payload: { id: id } };
+};
+
+// Reducers
+let id = 0
+
+export default function reducer(state =[], action) {
+    switch (action.type) {
+        case ADD_TASK:
             return [
                 ...state,
                 {   
                     id: id++,
                     text: action.payload.task,
                     completed: false
-                }
-            ]
-        case 'REMOVE_TASK':
-            return state.filter(task => task.id !== payload.action.id);
+                },
+            ];
+        case REMOVE_TASK:
+            return state.filter((task) => task.id !== action.payload.id);
+
+        case TASK_COMPLETED:
+            return state.map((task) =>
+                task.id === action.payload.id
+                    ? {
+                          ...task,
+                          completed: true,
+                      }
+                    : task
+            );
 
         default:
             return state
